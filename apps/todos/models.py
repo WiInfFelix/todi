dbd98b2bd94092b6dca4a61e43c756d167b09999
WiInfFelix <schefflerfelix95@gmail.com>
2022-10-12 12:45:04 +0000

    init project, no routes

---
+++ apps/todos/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class Todo(models.Model):
+
+    text = models.TextField(max_length=1024)
+    status = models.BooleanField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
---
+++ apps/todos/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from apps.todos.models import Todo
+
+from apps.todos.serializers import TodoSerializer
+# Create your views here.
+
+class TodoViewSet(viewsets.ModelViewSet):
+
+    serializer_class = TodoSerializer
+    queryset = Todo.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
+
+    def get_queryset(self):
+        return self.queryset.filter(created_by=self.request.user)
+        
